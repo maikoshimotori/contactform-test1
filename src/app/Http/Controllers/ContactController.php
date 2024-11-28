2fdf77919008cd9f6271ad269e9c163c2447b408
maiko <3430maiko@gmail.com>
2024-11-28 23:42:36 +0900

    ファイルの追加

---
+++ src/app/Http/Controllers/ContactController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ContactRequest;
+use App\Models\Contact;
+
+class ContactController extends Controller
+{
+    public function index()
+    {
+        return view('index');
+    }
+
+}
